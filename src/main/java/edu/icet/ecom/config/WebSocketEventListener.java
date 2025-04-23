88d3aa775c00ded205e0390b76aa3a0dde3a4ba6
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2025-04-23 15:45:28 +0530

    implemented websocket event listener

--- src/main/java/edu/icet/ecom/config/WebSocketEventListener.java
+++ src/main/java/edu/icet/ecom/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package edu.icet.ecom.config;
 
+import edu.icet.ecom.dto.ChatMessage;
+import edu.icet.ecom.util.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,8 +15,16 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
-    @EventListener
-    public void handleWebSocketDisconnectListener(){
-       // SessionDisconnectEvent event
+    private final SimpMessageSendingOperations messagingTemplate;
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headersAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headersAccessor.getSessionAttributes().get("username");
+
+        if (username!=null) {
+            log.info("User Disconnected: {}", username);
+            var chatMessage = ChatMessage.builder().type(MessageType.LEAVE).sender(username).build();
+
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }
